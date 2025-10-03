f8a6b9a67abf20d522ed596c7330174586f04f65
Cristian Salinas <salinas.cristian15@gmail.com>
2025-10-03 19:22:48 -0300

    Cristian Salinas

--- main.py
+++ main.py
@@ -14,3 +14,6 @@ def divide(a, b):
     if b == 0:
         raise ValueError("No se puede dividir por cero")
     return a / b
+
+def square(a):
+    return a * a
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from main import suma, resta, multiplica, divide
+from main import suma, resta, multiplica, divide, square
 
 
 def test_suma():
@@ -23,3 +23,7 @@ def test_divide():
     assert divide(3, 1) == 3
     with pytest.raises(ValueError):
         divide(5, 0)
+
+def test_square():
+    assert square(2) == 2
+    assert square(3) == 9
